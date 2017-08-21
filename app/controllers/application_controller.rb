ee4b68714f28260f21dfcc5e9923f606df1d3edf
Chris W Jones <chris@christopherjones.us>
2017-08-21 17:39:21 -0400

    Adds authorization to controllers
    I added a `before_action` to the `ApplicationController` that requires
    users to login before accessing pages by default.  I then explicitly
    skipped that requirement in the `BasicController` and
    `SessionController`.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+  before_action :require_login
 
   private
 
@@ -7,5 +9,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def require_login
+    unless current_user
+      redirect_to root_path, alert: "You must be logged in to access this section"
+    end
+  end
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,4 +1,5 @@
 class SessionController < ApplicationController
+  skip_before_action :require_login, only: [:create]
   def create
     auth = request.env["omniauth.auth"]
 
